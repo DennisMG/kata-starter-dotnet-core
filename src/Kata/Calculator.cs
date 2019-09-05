003f20bf4d88666a653f378c7b15504265dfc412
Isaac Martinez <imtz90b@gmail.com>
2019-09-05 12:04:15 -0600

    refactor

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -5,27 +5,22 @@ namespace Kata
 {
     public class Calculator
     {
-        public int Add(string input="")
+        public int Add(string input = "")
         {
             if (string.IsNullOrEmpty(input)) return 0;
-            var delimiters = new []{",", "\n"};
+            var delimiters = new[] {",", "\n"};
             var stringInput = input;
-            if (input.Length > 1)
+            if (input.StartsWith("//"))
             {
-                if (input.StartsWith("//"))
-                {
-                    var parts = input.Split("\n");
-                    delimiters = parts[0].Replace("//", "").Replace("[","").Split(']');
-                    stringInput = parts[1];
-                }
-                var numbers = stringInput.Split(delimiters, StringSplitOptions.None).Select(int.Parse);
-                var negative = numbers.Where(number => number < 0).ToArray();
-                if(negative.Any()) throw new Exception($"negatives not allowed: {string.Join(", ", negative)}"); 
-                return numbers.Where(number => number <= 1000).Sum();
+                var parts = input.Split("\n");
+                delimiters = parts[0].Replace("//", "").Replace("[", "").Split(']');
+                stringInput = parts[1];
             }
 
-            return int.Parse(input);
-
+            var numbers = stringInput.Split(delimiters, StringSplitOptions.None).Select(int.Parse);
+            var negative = numbers.Where(number => number < 0).ToArray();
+            if (negative.Any()) throw new Exception($"negatives not allowed: {string.Join(", ", negative)}");
+            return numbers.Where(number => number <= 1000).Sum();
         }
     }
 }
