d23008f043aaff6a3f31ac59013b755570037079
Dennis Molina <noexis@bvbvb.com>
2019-09-12 11:36:42 -0600

    custom delimiter passing, negative numbers failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -8,7 +8,14 @@ public class Calculator
         if (string.IsNullOrEmpty(input)) return 0;
 
         var delimiters = new [] {",","\n"};
-        var numbers = input.Split(delimiters, StringSplitOptions.None).Select(int.Parse).ToArray();
+        var newInput = input;
+        if (input.StartsWith("//"))
+        {
+            var parts = input.Split('\n');
+            delimiters = new[] { parts[0].Replace("//", "")};
+            newInput = parts[1];
+        }
+        var numbers = newInput.Split(delimiters, StringSplitOptions.None).Select(int.Parse).ToArray();
         if (numbers.Count() == 1) return numbers.First();
         return numbers.Sum();
     }
