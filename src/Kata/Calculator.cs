6d7c3b0c8c56a43d8a7e246045237ec671120c06
Dennis Molina <noexis@bvbvb.com>
2019-09-12 11:24:08 -0600

    multiple numbers passing, new line failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -8,6 +8,6 @@ public class Calculator
 
         var numbers = input.Split(",").Select(int.Parse).ToArray();
         if (numbers.Count() == 1) return numbers.First();
-        return numbers.First() + numbers.Last();
+        return numbers.Sum();
     }
 }
