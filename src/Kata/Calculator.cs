473a3923d659b800fecfaaa564960746b92a92fc
Isaac Martinez <imtz90b@gmail.com>
2019-09-17 12:01:48 -0600

    wrapping up

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -11,27 +11,35 @@ namespace Kata
 
             var delimiters = new [] {",","\n"};
             var newInput = input;
-            if (input.StartsWith("//"))
-            {
-                var parts = newInput.Split('\n');
-                delimiters = new []{ parts[0].Replace("//", "").Replace("[", "").Replace("]","")};
-                newInput = parts[1];
-                
-            }
+            newInput = extractcustomDelimiters(input, newInput, ref delimiters);
             
             var numbers = newInput.Split(delimiters, StringSplitOptions.None).Select((int.Parse)).Where(x=> x<=1000).ToArray();
             
             var negatives = numbers.Where(x => x < 0).ToArray();
+            validateNegatives(negatives);
+            
+            return numbers.Sum();
+
+        }
+
+        private static string extractcustomDelimiters(string input, string newInput, ref string[] delimiters)
+        {
+            if (input.StartsWith("//"))
+            {
+                var parts = newInput.Split('\n');
+                delimiters = parts[0].Replace("//", "").Replace("[", "").Split("]").ToArray();
+                newInput = parts[1];
+            }
+
+            return newInput;
+        }
+
+        private static void validateNegatives(int[] negatives)
+        {
             if (negatives.Any())
             {
                 throw new Exception($"negatives not allowed: {string.Join(", ", negatives)}");
             }
-            if (numbers.Length == 1)
-            {
-                return int.Parse(input);    
-            }
-            return numbers.Sum();
-
         }
     }
 }
