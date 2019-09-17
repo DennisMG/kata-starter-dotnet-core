29e51c2117d0465c310a25b3657f2241556339c3
Dennis Molina <noexis@bvbvb.com>
2019-09-17 11:34:51 -0600

    negative numbers failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -10,7 +10,17 @@ namespace Kata
             if (string.IsNullOrEmpty(input)) return 0;
 
             var delimiters = new [] {",","\n"};
-            var numbers = input.Split(delimiters, StringSplitOptions.None).Select((int.Parse)).ToArray();
+            var newInput = input;
+            if (input.StartsWith("//"))
+            {
+                var parts = newInput.Split('\n');
+                delimiters = new []{ parts[0].Replace("//", "")};
+                newInput = parts[1];
+                
+            }
+            
+            var numbers = newInput.Split(delimiters, StringSplitOptions.None).Select((int.Parse)).ToArray();
+
             if (numbers.Length == 1)
             {
                 return int.Parse(input);    
